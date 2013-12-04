1892c7dde8a593add2bf669e328594fe442e63cf
skparkes <bias.variance@gmail.com>
2013-12-04 00:42:34 -0500

    Nothing much done; just learned a lot.
    I came, I tried, I cried, I moved on.

--- 01_Import.py
+++ 01_Import.py
@@ -1,5 +1,24 @@
-from pandas import DataFrame
+# <codecell>
+import numpy as np
+from pandas import DataFrame, Series
 import pandas as pd
 
+# <codecell>
+#Run the following line once to define the data location on each machine
+#%bookmark london E:/GitHub/kaggle-london/
+%cd -b london
+%pwd
+
+
+# <codecell>
+a=5
+?a
+
+
+
+# <codecell>
 raw_train = pd.read_csv('Data_Raw/train.csv')
-print raw_train.head()
+#?raw_train
+raw_train
+#?raw_train
+#?raw_train.ix[0,0]
---
+++ Launch_IPython_Notebook.bat
@@ -0,0 +1,3 @@
+rem IPython must be in your system path
+rem Executing this batch in the path where IPython Notebook will look for projects
+ipython notebook --pylab=inline
