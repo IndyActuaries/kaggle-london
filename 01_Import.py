d79ee6c6ee5df7287c291be2c5fda550aa955592
skparkes <bias.variance@gmail.com>
2013-12-09 21:03:59 -0500

    Shift to just numpy for now
    Working my way to visualizations.

--- 01_Import.py
+++ 01_Import.py
@@ -1,24 +1,24 @@
 # <codecell>
+from __future__ import division, print_function
 import numpy as np
-from pandas import DataFrame, Series
-import pandas as pd
+import matplotlib.pyplot as plt
+#from pandas import DataFrame, Series
+#import pandas as pd
+
 
 # <codecell>
 #Run the following line once to define the data location on each machine
 #%bookmark london E:/GitHub/kaggle-london/
+%bookmark -l
 %cd -b london
 %pwd
 
 
 # <codecell>
-a=5
-?a
-
-
+feat_train = np.genfromtxt('Data_Raw/train.csv', delimiter=',')
+feat_test = np.genfromtxt('Data_Raw/test.csv', delimiter=',')
+resp_train = np.genfromtxt('Data_Raw/trainLabels.csv', delimiter=',')
 
 # <codecell>
-raw_train = pd.read_csv('Data_Raw/train.csv')
-#?raw_train
-raw_train
-#?raw_train
-#?raw_train.ix[0,0]
+feat_train_means = np.mean(feat_train, 1)
+#feat_train_means.
